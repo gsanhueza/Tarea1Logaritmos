6ae9262d1746d2d5167b3e6e7b05d82a4460684b
gsanhueza <gabriel_8032@hotmail.com>
2016-11-03 20:55:44 -0300

    fixes (?)

--- R_Trees_C/main.c
+++ R_Trees_C/main.c
@@ -2,18 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc,char **argv) {
+int main (int argc, char **argv) {
     printf("Hello World!\n");
     Rectangle *r = createRectangle(1, 2, 3, 4, "R");
-
-    Node *node = (Node * )malloc(sizeof(Node));
-    Rectangle **asdf = (Rectangle **)malloc(sizeof(Rectangle*));
 
     printf("X = %d\nY = %d\nAncho = %d\nAlto = %d\nId = %s\n\n", r->x, r->y, r->w, r->h, r->id);
 
     FILE *fp;
     char *c = (char *)r;
-    fp = fopen("test.tree","wb+");
+    fp = fopen("test.tree", "wb+");
     fwrite(r, sizeof(Rectangle), 1, fp);
 
     fclose(fp);
--- R_Trees_C/rectangle.c
+++ R_Trees_C/rectangle.c
@@ -1,6 +1,7 @@
 #include "rectangle.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 Rectangle* createRectangle(int x, int y, int w, int h, char *id) {
     Rectangle *rect = (Rectangle *)malloc(sizeof(Rectangle *));
@@ -10,10 +11,6 @@ Rectangle* createRectangle(int x, int y, int w, int h, char *id) {
     rect->h = h;
     rect->id = id;
     return rect;
-}
-
-int intersect(Rectangle *rect1, Rectangle *rect2){
-    return 0;
 }
 
 Node* createNode() {
@@ -43,10 +40,6 @@ Node** search(Node *node, Rectangle *rect) {
     return answer;
 }
 
-int* insertar(Rectangle rect) {
-    return 0;
-}
-
 void insertRectToNode(Node *n, Rectangle *r) {
     int i = 0;
     Node *header = n;
@@ -55,4 +48,55 @@ void insertRectToNode(Node *n, Rectangle *r) {
     *(n->rectArray) = r;
 
     n = header;
+}
+
+int intersect (Rectangle *r1, Rectangle *r2){
+    return r1->x <= r2->x + r2->w && r2->x <= r1->x + r1->w && r1->y <= r2->y + r2->h && r2->y <= r1->y + r1->h;
+}
+/* Calcula la nueva Area si se agrega r2 a r1.
+ *Retorna la diferencia de las Areas.*/
+int MBR(Rectangle *r1, Rectangle *r2) {
+    int min_x = r1->x < r2->x ? r1->x : r2->x;
+    int max_x = r1->x + r1->w > r2->x + r2->w ? r1->x + r1->w : r2->x + r2->w;
+    int min_y = r1->y < r2->y ? r1->y : r2->y;
+    int max_y = r1->x + r1->h > r2->x + r2->h ? r1->x + r1->h : r2->x + r2->h;
+    int r1_area = ( r1->w ) * ( r1->h );
+    int area = (max_x-min_x) * (max_y-min_y);
+    return area - r1_area;
+}
+
+/* Actualiza las coordenadas de r1 al agregarle r2, solo las actualiza no añade r2 a r1.*/
+void mergeRectangle(Rectangle *r1, Rectangle *r2) {
+    int max_x = r1->x + r1->w > r2->x + r2->w ? r1->x + r1->w : r2->x + r2->w;
+    int max_y = r1->x + r1->h > r2->x + r2->h ? r1->x + r1->h : r2->x + r2->h;
+    r1->x = r1->x < r2->x ? r1->x : r2->x;
+    r1->y = r1->y < r2->y ? r1->y : r2->y;
+    r1->w = max_x - r1->x ;
+    r1->h = max_y - r1->y;
+}
+
+void insertar ( Node *node , Rectangle *r ) {
+    if ( (node->rectArray)[0]->hijo != NULL ){
+        int minMBR = INT_MAX;
+        Rectangle *visitor;
+        for (int i = 0; i < node->size; i++ ) {
+            if( (node->rectArray)[i] == NULL )
+                break;
+            if ( MBR ( r, (node->rectArray)[i] ) < minMBR ){
+                minMBR = MBR ( r, (node->rectArray)[i] );
+                visitor = (node->rectArray)[i];
+
+            }
+        }
+        insertar( visitor->hijo, r);
+    }
+    else {
+
+        for (int i = 0; i< node->size; i ++ ){
+            if((node->rectArray)[i] == NULL){
+                (node->rectArray)[i] = r;
+                break;
+            }
+        }
+    }
 }
