cf402b9d2db0810eaaadac947e7d2b1eb6215abe
gsanhueza <gabriel_8032@hotmail.com>
2016-11-06 21:02:07 -0300

    Documentacion codigo

--- R_Trees_C/main.c
+++ R_Trees_C/main.c
@@ -4,24 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int randomNum(int max) {
-    return rand() % max;
-}
-
-Node *createTestRectangles(int n) {
-    Node *node = createNode();
-    char name[200];
-    for (int i = 0; i < n ; i ++ ){
-        sprintf(name, "Rectangle%d", i);
-        //puts(id);
-        Rectangle *rect = createRectangle(randomNum(10),randomNum(10),randomNum(10),randomNum(10),i);
-        printRectangle(rect);
-        node->rectArray[i]=rect;
-        node->size++;
-    }
-    return node;
-}
-
 int main(void) {
     srand(123);/*Inicializa el random, si no se cambia el parametro lanzara siempre la misma secuencia*/
 
