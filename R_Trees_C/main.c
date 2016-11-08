b62713e09fef04f14c7786b0f50025427edb7a9c
gsanhueza <gabriel_8032@hotmail.com>
2016-11-07 21:08:06 -0300

    ponerle nombre al header soluciona algunos problemas

--- R_Trees_C/main.c
+++ R_Trees_C/main.c
@@ -8,13 +8,14 @@
 int main(void) {
     srand(123); /* Inicializa el random, si no se cambia el parametro lanzara siempre la misma secuencia */
 
-    int numRectangles = 200; // 262144
+    int numRectangles = 2000; // 262144
     Rectangle **rects = bateriaRectangulos(numRectangles);
 
     Node *header = createNode();
     Rectangle *r = createRectangle(0, 0, 4, 5, -1);
     header->rectArray[0] = r;
     header->occupied = 1;
+    header->this_node_filename = "Nodes/Node0.bin"; // WHAT
 
     char *node = writeToDisk(header);
 
